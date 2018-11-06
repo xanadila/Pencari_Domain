3ea2349dc7dc57e84830a7e5ada5f95e138307d5
Ren <Ren@Laurensius>
2018-11-06 12:16:48 +0700

    version 1.5

---
+++ Pencari_Domain2/src/java/helper/UserDomainProviderHelper.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+import util.NewHibernateUtil;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import pojos.UserDomainProvider;
+
+public class UserDomainProviderHelper {
+
+    public UserDomainProviderHelper() {
+
+    }
+
+    public List<UserDomainProvider> getUserDomainProvider(){
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        String query = "from UserDomainProvider";
+        Query q = session.createQuery(query);
+        List<UserDomainProvider> list = q.list();
+        return list;
+    }
+
+}
+
+
