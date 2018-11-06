893c9e7b07f1ecf2beb0abc4ab8956965e57c5f6
Ren <Ren@Laurensius>
2018-11-06 11:00:18 +0700

    version 1.3

---
+++ Pencari_Domain2/src/java/test/TestUser.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import helper.UserCalonPembeliHelper;
+import java.util.List;
+
+import pojos.UserCalonPembeli;
+
+/**
+ *
+ * @author Ren
+ */
+public class TestUser {
+
+    public static void main(String[] args) {
+        UserCalonPembeliHelper helper = new UserCalonPembeliHelper();
+        List<UserCalonPembeli> list = helper.getUserCalonPembeli();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getEmail());
+            System.out.println(list.get(i).getNama());
+            System.out.println(list.get(i).getPassword());
+        }
+
+    }
+}
